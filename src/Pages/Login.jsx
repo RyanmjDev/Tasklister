e3b5868e6cc13b42f180e8d4ee808e167addf6a2
RyanmjDev <123763401+RyanmjDev@users.noreply.github.com>
2023-05-29 17:02:21 -0500

    Added Logo to login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,8 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import api from '../utils/api';
 import { checkLoggedIn } from '../utils/auth';
-
-// import LogoHorizontal from '../assets/Logo-Horizontal.svg'
+import LogoHorizontal from '../assets/Logo-Horizontal.svg';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -47,7 +46,9 @@ const Login = () => {
   return ( 
     <div className="flex md:justify-center md:items-center md:h-screen md:w-screen bg-gray-100">
       <div className="flex flex-col justify-center bg-white shadow-lg rounded-lg p-8 w-full md:max-w-xl md:w-5/6 md:rounded-md">
-        <h1 className="text-4xl text-center mb-8 font-bold"> Tasklister </h1>
+      <div className="flex justify-center mb-8">
+          <img src={LogoHorizontal}  alt="Logo" className="h-16" />
+        </div>
         {error && (
           <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-6 rounded-lg">
             <p>{error}</p>
