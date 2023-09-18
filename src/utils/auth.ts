fc14b6f73f363e76d3571b38b46f418d018c29fb
RyanmjDev <123763401+RyanmjDev@users.noreply.github.com>
2023-09-18 18:47:31 -0500

    update navigate function for typescript

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,19 +1,19 @@
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
-export const checkLoggedIn = (navigate:string) => {
+export const checkLoggedIn = (navigate: (path: string) => void) => {
   if (Cookies.get("token")) {
     navigate('/'); 
   } 
 };
 
-export const checkLoggedOut = (navigate:string) => {
+export const checkLoggedOut = (navigate: (path: string) => void) => {
     if (!Cookies.get("token")) {
         navigate('/login'); 
       } 
 }
 
-export const logout = (navigate:string) => {
+export const logout = (navigate: (path: string) => void) => {
   localStorage.removeItem('token');
   Cookies.remove('token');
   delete axios.defaults.headers.common['Authorization'];
